90e93bf20a8804385ae84f2b591a94ee22051722
Kim Tae Won <33194980+ktw1525@users.noreply.github.com>
2025-01-15 22:08:16 +0900

    filter updated

--- RCmeter/test_RLCfilter.m
+++ RCmeter/test_RLCfilter.m
@@ -2,13 +2,13 @@
 clear all
 clc
 
-fc = 60;
-w = 2*pi*fc;
-R = 1;
-BW = 20;
+fc = 60.155;
+wc = 2*pi*fc;
+R = 10;
+BW = 7.958*4;
 Q = fc / BW;
-L = Q / w * R
-C = 1/L/w/w
+L = Q / wc * R
+C = 1/L/wc/wc
 
 f = 1:0.1:500;
 w = 2*pi*f;
@@ -20,47 +20,21 @@ X = XL + XC;
 Z = R + i*X;
 I = abs(Vamp*ones(1,length(w))./Z);
 Vout = Vamp-I*R;
+
 P = I*Vamp;
 
-figure(1);
-subplot(3,2,1);
-plot(f, XL);
-title('Frequency - Reactance(X_L)');
-xlabel('Frequentcy [Hz]');
-ylabel('XL [Ohm]');
-subplot(3,2,2);
-plot(f, XC);
-title('Frequency - Reactance(X_C)');
-xlabel('Frequentcy [Hz]');
-ylabel('XC [Ohm]');
-subplot(3,2,3);
-plot(f, X);
-title('Frequency - Reactance(X_T_o_t_a_l)');
-xlabel('Frequentcy [Hz]');
-ylabel('X [Ohm]');
-subplot(3,2,4);
-plot(f, I);
-title('Frequency - Current [A]');
-xlabel('Frequentcy [Hz]');
-ylabel('Current [A]');
-subplot(3,2,5);
-plot(f, Vout);
-title('Frequency - Output Voltage [V]');
-xlabel('Frequentcy [Hz]');
-ylabel('Vout [V]');
-subplot(3,2,6);
-plot(f, P);
-title('Frequency - Power [W]');
-xlabel('Frequentcy [Hz]');
-ylabel('Power [W]');
-
-
 dt = 0.0001;
-t = 0:dt:2;
+t = 0:dt:2.499;
 fi = 60;
-fc = 60;
 wi = 2*pi*fi;
-V = Vamp*sin(wi*t) + Vamp*sin(wi/10*t) + Vamp/10*sin(wi*10*t);
+XLi = wi*L;
+XCi = -1/wi/C;
+Xi = XLi + XCi;
+Zi = R + i*Xi;
+Ii = abs(Vamp/Zi);
+Vouti = Vamp-Ii*R
+
+V = Vamp*sin(wi*t);% + Vamp*sin(wi/10*t) + Vamp/10*sin(wi*10*t);
 
 filter = struct();
 filter.dt = dt;
@@ -70,14 +44,61 @@ filter.BW = BW;
 filter.R = R;
 filter = RLC_series(filter);
 
+figure(1);
+subplot(3,2,1);
+plot(f, XL);
+title('Frequency - Reactance(X_L)');
+xlabel('Frequentcy [Hz]');
+ylabel('XL [Ohm]');
+grid on;
+subplot(3,2,2);
+plot(f, XC);
+title('Frequency - Reactance(X_C)');
+xlabel('Frequentcy [Hz]');
+ylabel('XC [Ohm]');
+grid on;
+subplot(3,2,3);
+plot(f, X);
+title('Frequency - Reactance(X_T_o_t_a_l)');
+xlabel('Frequentcy [Hz]');
+ylabel('X [Ohm]');
+grid on;
+subplot(3,2,4);
+plot(f, I);
+title('Frequency - Current [A]');
+xlabel('Frequentcy [Hz]');
+ylabel('Current [A]');
+grid on;
+subplot(3,2,5);
+plot(f, Vout, fi, Vouti, 'rx');
+title('Frequency - Output Voltage [V]');
+xlabel('Frequentcy [Hz]');
+ylabel('Vout [V]');
+text(fi, Vouti, sprintf('(%0.2f, %0.2f)', fi, Vouti));
+grid on;
+subplot(3,2,6);
+plot(f, P);
+title('Frequency - Power [W]');
+xlabel('Frequentcy [Hz]');
+ylabel('Power [W]');
+grid on;
+
 figure(2);
 subplot(2,1,1);
 plot(filter.input);
 title('Time - Input Voltage [A]');
 xlabel('Second [s]');
 ylabel('Voltage [V]');
+grid on;
 subplot(2,1,2);
 plot(filter.output);
 title('Time - Output Voltage [A]');
 xlabel('Second [s]');
 ylabel('Voltage [V]');
+grid on;
+
+
+Cx = 1.4*10^-4;
+Lx = 0.05;
+fx = 1/sqrt(Cx*Lx)/2/pi
+
